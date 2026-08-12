65896b25bfdf768257955b22f628f2a91a6a129a
Daniel Braikeh <5272396+danbraik@users.noreply.github.com>
2026-08-12 13:39:18 +0200

    add PlateNumber value object

---
+++ Backend/Node/Boilerplate/src/domain/valueObjects/PlateNumber.ts
@@ -0,0 +1,21 @@
+export class PlateNumber {
+  private constructor(private readonly value: string) { }
+
+  static create(value: string): PlateNumber {
+    const normalizedValue = value.trim();
+
+    if (normalizedValue.length === 0) {
+      throw new Error('PlateNumber cannot be empty.');
+    }
+
+    return new PlateNumber(normalizedValue);
+  }
+
+  equals(other: PlateNumber): boolean {
+    return this.value === other.value;
+  }
+
+  toString(): string {
+    return this.value;
+  }
+}
