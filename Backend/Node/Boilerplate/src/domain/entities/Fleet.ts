90024a32991729b6fc306fb09b8e61f228a12aab
Daniel Braikeh <5272396+danbraik@users.noreply.github.com>
2026-08-12 14:46:28 +0200

    refactor Fleet entity to handle already registered vehicles in addVehicule method

--- Backend/Node/Boilerplate/src/domain/entities/Fleet.ts
+++ Backend/Node/Boilerplate/src/domain/entities/Fleet.ts
@@ -1,6 +1,9 @@
+import { Result } from '../../shared/Result';
 import { FleetId } from '../valueObjects/FleetId';
 import { UserId } from '../valueObjects/UserId';
 import { VehiculeId } from '../valueObjects/VehiculeId';
+
+export class AlreadyRegisteredVehiculeError extends Error { }
 
 export class Fleet {
   private constructor(
@@ -25,8 +28,13 @@ export class Fleet {
     return this.vehiculeIds.some((fleetVehiculeId) => fleetVehiculeId.equals(vehiculeId));
   }
 
-  addVehicule(vehiculeId: VehiculeId): void {
+  addVehicule(vehiculeId: VehiculeId): Result<void> {
+    if (this.hasVehicule(vehiculeId)) {
+      return Result.fail(new AlreadyRegisteredVehiculeError());
+    }
+
     this.vehiculeIds.push(vehiculeId);
+    return Result.ok(undefined);
   }
 
   static create(id: FleetId, ownerId: UserId, vehiculeIds: VehiculeId[] = []): Fleet {
