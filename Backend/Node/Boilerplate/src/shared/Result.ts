32727f5f5c1fa7286041aa44798687394019e1cd
Daniel Braikeh <5272396+danbraik@users.noreply.github.com>
2026-08-12 13:37:55 +0200

    add util for Result pattern

---
+++ Backend/Node/Boilerplate/src/shared/Result.ts
@@ -0,0 +1,44 @@
+export type Result<T, E = Error> = Success<T, E> | Failure<T, E>;
+
+class Success<T, E> {
+  public readonly isSuccess = true as const;
+  public readonly isFailure = false as const;
+  public readonly error = null;
+  private readonly _value: T;
+
+  constructor(value: T) {
+    this._value = value;
+  }
+
+  public getValue(): T {
+    return this._value;
+  }
+}
+
+class Failure<T, E> {
+  public readonly isSuccess = false as const;
+  public readonly isFailure = true as const;
+  public readonly error: E;
+
+  constructor(error: E) {
+    this.error = error;
+  }
+
+  public getValue(): T {
+    throw new Error("Can't get value from failure result");
+  }
+}
+
+export const Result = {
+  ok<T, E = Error>(value: T): Result<T, E> {
+    return new Success(value);
+  },
+
+  fail<T, E = Error>(error: E): Result<T, E> {
+    return new Failure(error);
+  },
+
+  failWithMessage(message: string): Result<never, Error> {
+    return new Failure(new Error(message));
+  },
+};
