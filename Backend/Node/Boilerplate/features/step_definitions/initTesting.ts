115b83371e1c5872e644f9404bd56ccdfccf4fdd
Daniel Braikeh <5272396+danbraik@users.noreply.github.com>
2026-08-12 19:24:29 +0200

    feat: enhance testing setup with Before and After hooks for critical scenarios

--- Backend/Node/Boilerplate/features/step_definitions/initTesting.ts
+++ Backend/Node/Boilerplate/features/step_definitions/initTesting.ts
@@ -1,4 +1,23 @@
-import { setWorldConstructor } from "@cucumber/cucumber";
+import { After, Before, setWorldConstructor } from "@cucumber/cucumber";
 import { TestingWorld } from "./TestingWorld";
+import { initializeDatabase } from "../../src/infra/database/initialize";
+import { SqlFleetRepository } from "../../src/infra/repositories/SqlFleetRepository";
+import { SqlVehicleRepository } from "../../src/infra/repositories/SqlVehicleRepository";
 
 setWorldConstructor(TestingWorld);
+
+Before({ tags: "@critical" }, function () {
+    const that = this as TestingWorld;
+    that.database = initializeDatabase(":memory:");
+    that.setRepositories(
+        new SqlFleetRepository(that.database),
+        new SqlVehicleRepository(that.database),
+    );
+});
+
+After({ tags: "@critical" }, function () {
+    const that = this as TestingWorld;
+    if (that.database) {
+        that.database.close();
+    }
+});
