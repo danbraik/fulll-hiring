b92d24c5dc2ad3f154bb76259991f279c551966d
Daniel Braikeh <5272396+danbraik@users.noreply.github.com>
2026-08-12 14:30:14 +0200

    add TestingWorld step definition for fleet creation

--- Backend/Node/Boilerplate/features/step_definitions/TestingWorld.ts
+++ Backend/Node/Boilerplate/features/step_definitions/TestingWorld.ts
@@ -2,10 +2,16 @@ import { World, IWorldOptions } from '@cucumber/cucumber';
 import { FleetRepository } from '../../src/domain/repositories/FleetRepository';
 import { InMemoryFleetRepository } from '../../src/infra/repositories/InMemoryFleetRepository';
 import { Fleet } from '../../src/domain/entities/Fleet';
+import { CreateFleetCommandHandler } from '../../src/app/useCases/commands/createFleet/CreateFleetCommandHandler';
 
 export class TestingWorld extends World {
+    fleetRepository: FleetRepository;
+    createFleetCommandHandler: CreateFleetCommandHandler;
+    myFleet?: Fleet;
 
     constructor(options: IWorldOptions<any>) {
         super(options);
+        this.fleetRepository = new InMemoryFleetRepository();
+        this.createFleetCommandHandler = new CreateFleetCommandHandler(this.fleetRepository);
     }
 }
--- Backend/Node/Boilerplate/features/step_definitions/stepdefs.ts
+++ Backend/Node/Boilerplate/features/step_definitions/stepdefs.ts
@@ -1,3 +1,15 @@
 import assert from 'assert';
 import { Given, When, Then } from '@cucumber/cucumber';
 import { TestingWorld } from './TestingWorld';
+
+
+Given('my fleet', async function () {
+  const that = this as TestingWorld;
+
+  const handlerResult = await that.createFleetCommandHandler.handle({ userId: 'user-1' });
+  if (handlerResult.isFailure) {
+    throw new Error('Failed to create fleet');
+  }
+
+  that.myFleet = handlerResult.getValue();
+});
